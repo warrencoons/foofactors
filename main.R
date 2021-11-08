6c3efe1b313051843fbac85cff37e177da9d52c5
Warren Coons <WCoons@mednet.ucla.edu>
2021-11-07 23:20:13 -0800

    Initial commit

---
+++ main.R
@@ -0,0 +1,9 @@
+library(devtools) #includes a variety of functions that facilitate package 
+                  #requires that you use roxygen2 and testthat for testing
+library(tidyverse) #system of packages for data manipulation, exploration, and visualization
+library(fs) #?
+
+
+create_package("/Users/warrencoons/Desktop/R/Epi/building_package/to/foofactors")
+
+getwd()
